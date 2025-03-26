1c5775b3d29b6c66f6bd240385cb65fc9170cd7b
Romcodewasunavailable <120272499+Romcodewasunavailable@users.noreply.github.com>
2025-03-26 20:34:53 +0100

    Update dialog.py

--- game/dialog.py
+++ game/dialog.py
@@ -1,17 +1,25 @@
 """
-Système d'affichage de dialogues.
-WIP
+Système d'affichage d'interface.
+
+Contributors:
+    Romain
 """
 
+from common import Stats, DamageInstance, Character, Action
 import copy
 import curses
 import ctypes
 import math
 import time
-from typing import NamedTuple
+from typing import NamedTuple, Self
 
 X_CORRECTION = 2.6 # Hauteur / largeur d'un caractère
 CHARACTER_TIME = 0.025 # Délai d'affichage de chaque caractère dans les textes
+
+
+class DialogLine(NamedTuple):
+    text: str
+    character: Character = None
 
 
 class Rectangle(NamedTuple):
@@ -72,65 +80,79 @@ class DialogBox(NamedTuple):
     x: int
     height: int
     width: int
-    text: tuple[str, ...]
+    dialog: tuple[DialogLine, ...]
     start_time: float
-    text_part: int = 0
+    line_index: int = 0
+    
+    @property
+    def dialog_line(self) -> DialogLine:
+        return self.dialog[self.line_index]
     
     @classmethod
-    def new(cls, y: int, x: int, height: int, width: int, text: str | tuple[str, ...]) -> "DialogBox":
+    def new(cls, y: int, x: int, height: int, width: int, dialog: tuple[DialogLine, ...]) -> Self:
         return cls(
             y,
             x,
             height,
             width,
-            (text,) if isinstance(text, str) else text,
+            dialog,
             time.time(),
         )
     
-    def set_geometry(self, **kwargs) -> "DialogBox":
+    def set_geometry(self, **kwargs) -> Self:
         return DialogBox(
             kwargs["y"] if "y" in kwargs else self.y,
             kwargs["x"] if "x" in kwargs else self.x,
             kwargs["height"] if "height" in kwargs else self.height,
             kwargs["width"] if "width" in kwargs else self.width,
-            self.text,
+            self.dialog,
             self.start_time,
-            self.text_part,
+            self.line_index,
         )
     
     def next(self) -> "DialogBox":
-        if math.floor((time.time() - self.start_time) / CHARACTER_TIME) <= len(self.text[self.text_part]):
+        if math.floor((time.time() - self.start_time) / CHARACTER_TIME) <= len(self.dialog[self.line_index].text):
             return DialogBox(
                 self.y,
                 self.x,
                 self.height,
                 self.width,
-                self.text,
+                self.dialog,
                 0,
-                self.text_part,
+                self.line_index,
             )
-        elif self.text_part + 1 >= len(self.text):
-            return self
-        else:
+        elif self.line_index + 1 < len(self.dialog):
             return DialogBox(
                 self.y,
                 self.x,
                 self.height,
                 self.width,
-                self.text,
+                self.dialog,
                 time.time(),
-                self.text_part + 1,
+                self.line_index + 1,
             )
+        else:
+            return None
     
     def draw(self, buffer: list[list[tuple[str, int]]]) -> None:
-        length_to_draw = min(math.floor((time.time() - self.start_time) / CHARACTER_TIME), len(self.text[self.text_part]))
+        length_to_draw = min(math.floor((time.time() - self.start_time) / CHARACTER_TIME), len(self.dialog[self.line_index].text))
+        dialog_line = self.dialog[self.line_index]
         TextBox(
             self.y,
             self.x,
             self.height,
             self.width,
-            self.text[self.text_part][:length_to_draw]
-        ).draw(buffer) 
+            ((f"[{self.dialog_line.character.name}]: " if not self.dialog_line.character is None else "")
+            + self.dialog[self.line_index].text[:length_to_draw]),
+        ).draw(buffer)
+
+
+class ChoiceBox(NamedTuple):
+    y: int
+    x: int
+    height: int
+    width: int
+    options: tuple[str, ...]
 
 
 def move_toward(a: int | float, b: int | float, step: int | float = 1) -> int | float:
@@ -199,18 +221,18 @@ def main(stdscr) -> None:
     frame_count = 0
     average_fps = 0
     
-    text = (
-        "LELOLELOELOLEOLEOLEOLEOLEOLOLEOLOEELOmmmmmmmmmmmmmmmmmm    yeseiurrrrrhjsdhdjhsdjhsdhjsdhjdshjsdjhsdjhdsjhdshjsdhjsdhjsdhjdshjdshdsdssjhgfqwè¨qè¨¨èwq¨qwèwq",
-        "bruh",
-        "AAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-        "We're no strangers to love You know the rules and so do I A full commitment's what I'm thinkin' of You wouldn't get this from any other guy I just wanna tell you how I'm feeling Gotta make you understand Never gonna give you up, never gonna let you down Never gonna run around and desert you Never gonna make you cry, never gonna say goodbye Never gonna tell a lie and hurt you We've known each other for so long Your heart's been aching, but you're too shy to say it Inside, we both know what's been going on We know the game and we're gonna play it And if you ask me how I'm feeling Don't tell me you're too blind to see Never gonna give you up, never gonna let you down Never gonna run around and desert you Never gonna make you cry, never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up, never gonna let you down Never gonna run around and desert you Never gonna make you cry, never gonna say goodbye Never gonna tell a lie and hurt you We've known each other for so long Your heart's been aching, but you're too shy to say it Inside, we both know what's been going on We know the game and we're gonna play it I just wanna tell you how I'm feeling Gotta make you understand Never gonna give you up, never gonna let you down Never gonna run around and desert you Never gonna make you cry, never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up, never gonna let you down Never gonna run around and desert you Never gonna make you cry, never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up, never gonna let you down Never gonna run around and desert you Never gonna make you cry, never gonna say goodbye Never gonna tell a lie and hurt you",
+    dialog = (
+        DialogLine("LELOLELOELOLEOLEOLEOLEOLEOLOLEOLOEELOmmmmmmmmmmmmmmmmmm    yeseiurrrrrhjsdhdjhsdjhsdhjsdhjdshjsdjhsdjhdsjhdshjsdhjsdhjsdhjdshjdshdsdssjhgfqwè¨qè¨¨èwq¨qwèwq", Character("Idris")),
+        DialogLine("bruh"),
+        DialogLine("AAAAAAAAAAAAAAAAAAAAAAAAAAAAA"),
+        DialogLine("We're no strangers to love You know the rules and so do I A full commitment's what I'm thinkin' of You wouldn't get this from any other guy I just wanna tell you how I'm feeling Gotta make you understand Never gonna give you up, never gonna let you down Never gonna run around and desert you Never gonna make you cry, never gonna say goodbye Never gonna tell a lie and hurt you We've known each other for so long Your heart's been aching, but you're too shy to say it Inside, we both know what's been going on We know the game and we're gonna play it And if you ask me how I'm feeling Don't tell me you're too blind to see Never gonna give you up, never gonna let you down Never gonna run around and desert you Never gonna make you cry, never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up, never gonna let you down Never gonna run around and desert you Never gonna make you cry, never gonna say goodbye Never gonna tell a lie and hurt you We've known each other for so long Your heart's been aching, but you're too shy to say it Inside, we both know what's been going on We know the game and we're gonna play it I just wanna tell you how I'm feeling Gotta make you understand Never gonna give you up, never gonna let you down Never gonna run around and desert you Never gonna make you cry, never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up, never gonna let you down Never gonna run around and desert you Never gonna make you cry, never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up, never gonna let you down Never gonna run around and desert you Never gonna make you cry, never gonna say goodbye Never gonna tell a lie and hurt you", Character("Rick Astley")),
     )
     dialog_box = DialogBox.new(
         screen_height,
         50,
         10,
         screen_width - 100,
-        text,
+        dialog,
     )
     
     float_dialog_y = float(dialog_box.y)
@@ -228,7 +250,7 @@ def main(stdscr) -> None:
 
         key = stdscr.getch()
         if key == ord(" ") or key == ord("\n"):
-            dialog_box = dialog_box.next()
+            if not dialog_box is None: dialog_box = dialog_box.next()
         elif key == ord("q"):
             break
 
@@ -236,11 +258,12 @@ def main(stdscr) -> None:
 
         buffer = copy.deepcopy(empty_buffer)
         
-        dialog_target_y = screen_height if dialog_box.start_time is None else screen_height - 12
-        if dialog_box.y != dialog_target_y:
-            float_dialog_y = move_toward(float_dialog_y, dialog_target_y, delta_time * 100)
-            dialog_box = dialog_box.set_geometry(y=round(float_dialog_y))
-        dialog_box.draw(buffer)
+        if not dialog_box is None:
+            dialog_target_y = screen_height if dialog_box.start_time is None else screen_height - 12
+            if dialog_box.y != dialog_target_y:
+                float_dialog_y = move_toward(float_dialog_y, dialog_target_y, delta_time * 100)
+                dialog_box = dialog_box.set_geometry(y=round(float_dialog_y))
+            dialog_box.draw(buffer)
         
         display_buffer(stdscr, buffer)
         stdscr.addstr(0, 0, f"FPS : {round(average_fps)}")

68feaf503ebf70b672d6aba471e27d95c7dea327
Romain <120272499+Romcodewasunavailable@users.noreply.github.com>
2025-04-01 14:01:00 +0200

    Add files via upload

--- game/main.py
+++ game/main.py
@@ -1,7 +1,8 @@
 """Main game script
 
 Run with CTRL+T (thonny), currently contains game loop setup, utility labels and
-interface tests.
+interface tests. If _curses module missing, run pip install windows-curses in
+terminal.
 
 Contributors:
     Romain
@@ -36,14 +37,30 @@ dialog = (
     DialogLine("We're no strangers to love You know the rules and so do I A full commitment's what I'm thinkin' of You wouldn't get this from any other guy I just wanna tell you how I'm feeling Gotta make you understand Never gonna give you up, never gonna let you down Never gonna run around and desert you Never gonna make you cry, never gonna say goodbye Never gonna tell a lie and hurt you We've known each other for so long Your heart's been aching, but you're too shy to say it Inside, we both know what's been going on We know the game and we're gonna play it And if you ask me how I'm feeling Don't tell me you're too blind to see Never gonna give you up, never gonna let you down Never gonna run around and desert you Never gonna make you cry, never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up, never gonna let you down Never gonna run around and desert you Never gonna make you cry, never gonna say goodbye Never gonna tell a lie and hurt you We've known each other for so long Your heart's been aching, but you're too shy to say it Inside, we both know what's been going on We know the game and we're gonna play it I just wanna tell you how I'm feeling Gotta make you understand Never gonna give you up, never gonna let you down Never gonna run around and desert you Never gonna make you cry, never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up, never gonna let you down Never gonna run around and desert you Never gonna make you cry, never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up, never gonna let you down Never gonna run around and desert you Never gonna make you cry, never gonna say goodbye Never gonna tell a lie and hurt you", Character("Rick Astley")),
 )
 
+options = (
+    "haram",
+    "harambe",
+    "PETAH",
+    "The honse is here.",
+)
+
 ############ Code to run on startup goes here
 
-dialog_box = cuinter.DialogBox.new(
+dialog_box = None
+# dialog_box = cuinter.DialogBox.new(
+#     cuinter.screen_height - 12,
+#     cuinter.screen_width // 4,
+#     10,
+#     cuinter.screen_width // 2,
+#     dialog,
+# )
+
+choice_box = cuinter.ChoiceBox.new(
     cuinter.screen_height - 12,
     cuinter.screen_width // 4,
     10,
     cuinter.screen_width // 2,
-    dialog,
+    options,
 )
 
 ############
@@ -66,22 +83,25 @@ while 1:
     
     key = cuinter.get_key()
     
-    if key == ord(" ") or key == ord("\n"):
+    if key in (ord(" "), ord("\n")):
         if not dialog_box is None:
             dialog_box = dialog_box.next()
+        elif not choice_box is None:
+            choice = choice_box.confirm()
+            choice_box = None
     
-    elif key == cuinter.curses.KEY_UP:
+    elif key in (cuinter.curses.KEY_UP, ord("w")):
         if not choice_box is None:
             choice_box = choice_box.select_previous()
     
-    elif key == cuinter.curses.KEY_DOWN:
+    elif key in (cuinter.curses.KEY_DOWN, ord("s")):
         if not choice_box is None:
             choice_box = choice_box.select_next()
     
-    elif key == cuinter.curses.KEY_LEFT:
+    elif key in (cuinter.curses.KEY_LEFT, ord("a")):
         pass
     
-    elif key == cuinter.curses.KEY_RIGHT:
+    elif key in (cuinter.curses.KEY_RIGHT, ord("d")):
         pass
         
     elif key == ord("q"):
